5c6c825e0f0ca39ed9b27aab150501d5f907810e
Dicot0721 <149964080+Dicot0721@users.noreply.github.com>
2025-06-27 13:48:17 +0800

    style: 将制表符转换为空格

--- src/extension.ts
+++ src/extension.ts
@@ -23,73 +23,73 @@ let avz = new Avz();
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	const closeTerminal = vscode.window.onDidCloseTerminal(terminal => {
-		// Watch for when the server terminal closes.
-		if (terminal.name === "AvZ") {
-			avz.closeTerminal();
-		}
-	});
+    const closeTerminal = vscode.window.onDidCloseTerminal(terminal => {
+        // Watch for when the server terminal closes.
+        if (terminal.name === "AvZ") {
+            avz.closeTerminal();
+        }
+    });
 
 
-	const runScriptMaskCmd = vscode.commands.registerCommand("AsmVsZombies.runScript", () => {
-		avz.runScriptMaskCmd();
-	});
+    const runScriptMaskCmd = vscode.commands.registerCommand("AsmVsZombies.runScript", () => {
+        avz.runScriptMaskCmd();
+    });
 
-	const runScriptInTerminal = vscode.commands.registerCommand("AsmVsZombies.runScriptInTerminal", () => {
-		avz.runScriptInTerminal();
-	});
+    const runScriptInTerminal = vscode.commands.registerCommand("AsmVsZombies.runScriptInTerminal", () => {
+        avz.runScriptInTerminal();
+    });
 
-	const updateAvz = vscode.commands.registerCommand("AsmVsZombies.updateAvz", () => {
-		avz.updateAvz();
-	});
+    const updateAvz = vscode.commands.registerCommand("AsmVsZombies.updateAvz", () => {
+        avz.updateAvz();
+    });
 
-	const openAvzTutorial = vscode.commands.registerCommand("AsmVsZombies.openAvzTutorial", () => {
-		return vscode.env.openExternal(vscode.Uri.parse("https://gitee.com/vector-wlc/AsmVsZombies"));
-	});
+    const openAvzTutorial = vscode.commands.registerCommand("AsmVsZombies.openAvzTutorial", () => {
+        return vscode.env.openExternal(vscode.Uri.parse("https://gitee.com/vector-wlc/AsmVsZombies"));
+    });
 
-	const getPvzExePath = vscode.commands.registerCommand("AsmVsZombies.getPvzExePath", () => {
-		return avz.getPvzExePath();
-	});
+    const getPvzExePath = vscode.commands.registerCommand("AsmVsZombies.getPvzExePath", () => {
+        return avz.getPvzExePath();
+    });
 
-	const getPvzProcessId = vscode.commands.registerCommand("AsmVsZombies.getPvzProcessId", () => {
-		return avz.getPvzProcessId();
-	});
+    const getPvzProcessId = vscode.commands.registerCommand("AsmVsZombies.getPvzProcessId", () => {
+        return avz.getPvzProcessId();
+    });
 
-	const setAvzDir = vscode.commands.registerCommand("AsmVsZombies.setAvzDir", () => {
-		const options: vscode.OpenDialogOptions = {
-			canSelectFolders: true,
-			canSelectFiles: false,
-			canSelectMany: false,
-			openLabel: vscode.l10n.t("Open the AvZ installation directory")
-		};
+    const setAvzDir = vscode.commands.registerCommand("AsmVsZombies.setAvzDir", () => {
+        const options: vscode.OpenDialogOptions = {
+            canSelectFolders: true,
+            canSelectFiles: false,
+            canSelectMany: false,
+            openLabel: vscode.l10n.t("Open the AvZ installation directory")
+        };
 
-		vscode.window.showOpenDialog(options).then(dir => {
-			if (dir && dir.length > 0) {
-				avz.setAvzDir(dir[0].fsPath);
-			}
-		});
-	});
+        vscode.window.showOpenDialog(options).then(dir => {
+            if (dir && dir.length > 0) {
+                avz.setAvzDir(dir[0].fsPath);
+            }
+        });
+    });
 
-	const getAvzExtension = vscode.commands.registerCommand("AsmVsZombies.getAvzExtension", () => {
-		avz.getAvzExtension();
-	});
+    const getAvzExtension = vscode.commands.registerCommand("AsmVsZombies.getAvzExtension", () => {
+        avz.getAvzExtension();
+    });
 
-	const buildAvz = vscode.commands.registerCommand("AsmVsZombies.buildAvz", () => {
-		avz.buildAvz();
-	});
+    const buildAvz = vscode.commands.registerCommand("AsmVsZombies.buildAvz", () => {
+        avz.buildAvz();
+    });
 
-	context.subscriptions.push(
-		runScriptMaskCmd,
-		runScriptInTerminal,
-		updateAvz,
-		closeTerminal,
-		openAvzTutorial,
-		setAvzDir,
-		getPvzExePath,
-		getPvzProcessId,
-		getAvzExtension,
-		buildAvz
-	);
+    context.subscriptions.push(
+        runScriptMaskCmd,
+        runScriptInTerminal,
+        updateAvz,
+        closeTerminal,
+        openAvzTutorial,
+        setAvzDir,
+        getPvzExePath,
+        getPvzProcessId,
+        getAvzExtension,
+        buildAvz
+    );
 }
 
 // this method is called when your extension is deactivated
