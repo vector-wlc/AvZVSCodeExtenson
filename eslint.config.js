fe2abf746c43e91cd7e7478bee62f9dca4b2882c
Dicot0721 <149964080+Dicot0721@users.noreply.github.com>
2025-12-10 15:56:37 +0800

    chore: Migrate '.eslintrc' to 'eslint.config.js'
    - See:
      * https://eslint.org.cn/docs/latest/use/configure/migration-guide
      * https://www.npmjs.com/package/@eslint/migrate-config

---
+++ eslint.config.js
@@ -0,0 +1,54 @@
+import { defineConfig } from "eslint/config";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+
+export default defineConfig([{
+    files: ["src/**/*.ts"],
+
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+    },
+
+    languageOptions: {
+        parser: tsParser,
+        ecmaVersion: 6,
+        sourceType: "module",
+    },
+
+    rules: {
+        "@typescript-eslint/naming-convention": [
+            "warn",
+            {
+                selector: "default",
+                format: ["camelCase"],
+            },
+            {
+                selector: "variable",
+                format: ["camelCase", "UPPER_CASE"],
+                modifiers: ["const"],
+            },
+            {
+                selector: "parameter",
+                format: ["camelCase"],
+                leadingUnderscore: "allow",
+            },
+            {
+                selector: "typeLike",
+                format: ["PascalCase"],
+            },
+            {
+                selector: "enumMember",
+                format: ["UPPER_CASE"],
+            },
+            {
+                selector: "import",
+                format: ["camelCase", "PascalCase"],
+                modifiers: ["default"],
+            },
+        ],
+        curly: "warn",
+        eqeqeq: "warn",
+        "no-throw-literal": "warn",
+        semi: "off",
+    },
+}]);
